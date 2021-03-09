fab0843c602296b4d14f9f37313b88d35bada8fe
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-03-09 20:17:28 +0000

    Ejercicio 1 - Codigo hecho y expectativa correcta

--- src/ejercicio-1.ts
+++ src/ejercicio-1.ts
@@ -9,7 +9,22 @@
  */
 // export es para que la funcion se use en otros ficheros
 
-export function decodeResistor(color: string[]): number {
+export function decodeResistor(resistor: string[]) {
+  const colors:[string, number][] = [["Negro", 0], ["Marron", 1], ["Rojo", 2], ["Naranja", 3], ["Amarillo", 4],
+    ["Verde", 5], ["Azul", 6], ["Violeta", 7], ["Gris", 8], ["Blanco", 9]];
 
+  let output: number = 0;
+  let i: number = 0;
+  let j: number = 0;
+
+  while (i < colors.length && j < 2) {
+    if ( colors[i][0] == resistor[j]) {
+      output += colors[i][1];
+      i = -1;
+      j++;
+    }
+    i++;
+  }
+  return output;
 }
 
--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -3,11 +3,11 @@ import {expect} from 'chai';
 import {decodeResistor} from '../src/ejercicio-1';
 
 
-describe('valor resistencias:', () => {
-  it('resistencia 1:', () => {
-    expect(decodeResistor(`rojo`, `violeta`)).to.be.equal(9);
+describe('| VERIFICAR VALOR DE LAS RESISTENCIAS |', () => {
+  it('resistencia 1', () => {
+    expect(decodeResistor(["Rojo", "Violeta"])).to.be.equal(9);
   });
-  it('resistencia 2:', () => {
-    expect(decodeResistor(`amarillo`, `blanco`, `negro`)).to.be.equal(9);
+  it('resistencia 2', () => {
+    expect(decodeResistor(["Amarillo", "Blanco", "Gris"])).to.be.equal(13);
   });
 });
