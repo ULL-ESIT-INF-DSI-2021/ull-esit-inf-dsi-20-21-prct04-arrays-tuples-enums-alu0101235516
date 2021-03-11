28b01530271f8cedfb92052af18758c2eeb3a874
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-03-11 13:52:27 +0000

    Ejercicio 7 - Codigo hecho y expectativa funcionando

---
+++ src/ejercicio-7.ts
@@ -0,0 +1,75 @@
+/**
+ * Puntos bi-dimensionales.
+ * @param PrimerPunto Primer punto de coordenadas.
+ * @param SegundoPunto Segundo punto de coordenadas.
+ * @param cons Constante por la que multiplicamos el punto de coordenadas en la funcion producto.
+ * @return Devuelve un punto de coordenadas nuevo, ya sea despues de hacer la suma, resta, producto o la euclidea.
+ * ```ts
+ * sumar([4, 2, 5, 7], [1, 2, 3, 2]) -> [5, 4, 8, 9]
+ * restar([4, 2, 5, 7], [1, 2, 3, 2]) -> [3, 0, 2, 5]
+ * producto([4, 2, 5, 7], 2)) -> [8, 4, 10, 14]
+ * euclidea([4, 2, 5, 7], [1, 2, 3, 2]) -> (5)
+ * ```
+ */
+
+type Point = [number, number, number, ...number[]]
+
+export function sumar(PrimerPunto: Point, SegundoPunto: Point) {
+  const PointResult: Point = [0, 0, 0];
+
+  if (PrimerPunto.length == SegundoPunto.length) {
+    let i: number = 0;
+    PrimerPunto.forEach((element) => {
+      PointResult[i] = element + SegundoPunto[i];
+      i++;
+    });
+    return PointResult;
+  } else {
+    return "ERROR: Los puntos tienen que tener el mismo tamaño!";
+  }
+}
+
+export function restar(PrimerPunto: Point, SegundoPunto: Point) {
+  const PointResult: Point = [0, 0, 0];
+
+  if (PrimerPunto.length == SegundoPunto.length) {
+    let i: number = 0;
+    PrimerPunto.forEach((element) => {
+      PointResult[i] = element - SegundoPunto[i];
+      i++;
+    });
+    return PointResult;
+  } else {
+    return "ERROR: Los puntos tienen que tener el mismo tamaño!";
+  }
+}
+
+export function producto(PrimerPunto: Point, cons: number) {
+  const PointResult: Point = [0, 0, 0];
+
+  let i: number = 0;
+  PrimerPunto.forEach((element) => {
+    PointResult[i] = element * cons;
+    i++;
+  });
+  return PointResult;
+}
+
+export function euclidea(PrimerPunto: Point, SegundoPunto: Point) {
+  let pt1Pow: number = 0;
+  let distancia: number = 0;
+
+  if (PrimerPunto.length == SegundoPunto.length) {
+    let i: number = 0;
+    SegundoPunto.forEach((element) => {
+      pt1Pow = Math.pow((element - PrimerPunto[i]), 2);
+      i++;
+    });
+    distancia = Math.sqrt(pt1Pow);
+    return parseFloat(distancia.toFixed(4));
+  } else {
+    return "ERROR: Los puntos tienen que tener el mismo tamaño!";
+  }
+}
+
+console.log(euclidea([4, 2, 5, 7], [1, 2, 3, 2]));
--- tests/ejercicio-7.spec.ts
+++ tests/ejercicio-7.spec.ts
@@ -1,22 +1,22 @@
 import 'mocha';
 import {expect} from 'chai';
-import {sumar} from '../src/ejercicio-6';
-import {restar} from '../src/ejercicio-6';
-import {producto} from '../src/ejercicio-6';
-import {euclidea} from '../src/ejercicio-6';
+import {sumar} from '../src/ejercicio-7';
+import {restar} from '../src/ejercicio-7';
+import {producto} from '../src/ejercicio-7';
+import {euclidea} from '../src/ejercicio-7';
 
 
-describe('| EJ 6 - PUNTO BI-DIMENSIONAL |', () => {
+describe('| EJ 7 - PUNTO N-DIMENSIONAL |', () => {
   it('Suma de los puntos', () => {
-    expect(sumar([4, 2], [1, 3])).to.deep.equal([5, 5]);
+    expect(sumar([4, 2, 5, 7], [1, 2, 3, 2])).to.deep.equal([5, 4, 8, 9]);
   });
   it('Resta de los puntos', () => {
-    expect(restar([4, 2], [1, 3])).to.deep.equal([3, -1]);
+    expect(restar([4, 2, 5, 7], [1, 2, 3, 2])).to.deep.equal([3, 0, 2, 5]);
   });
   it('Producto de un punto', () => {
-    expect(producto([4, 2], 2)).to.deep.equal([8, 4]);
+    expect(producto([4, 2, 5, 7], 2)).to.deep.equal([8, 4, 10, 14]);
   });
   it('Euclidea entre puntos', () => {
-    expect(euclidea([4, 2], [1, 3])).to.deep.equal(3.1623);
+    expect(euclidea([4, 2, 5, 7], [1, 2, 3, 2])).to.deep.equal(5);
   });
 });
