2d0969eb8f573b40d38df0e17d578fdc3e5840a3
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-03-14 14:24:43 +0000

    Ejercicio 2 - Codigo hecho y expectativa funcionando

---
+++ src/ejercicio-2.ts
@@ -0,0 +1,32 @@
+/**
+ * Palabras encadenadas en un array.
+ * @param palStr Array con las palabras que vamos a concatenar.
+ * @returns Devuelve el encadenamiento realizado.
+ * ```TypeScript
+ * meshArray([“allow”, “lowering”, “ringmaster”, “terror”]) -> "lowringter"
+ * ```
+ */
+ export function meshArray(palStr: string[]) {
+  let output: string = ``;
+  for (let i: number = 0; i < palStr.length - 1; i++) {
+    const palb: string = palStr[i];
+    const nextPalb: string = palStr[i + 1];
+    let pos: number= palb.length - 1;
+    let posnextPalb: number = 0;
+    while (palb[pos] != nextPalb[posnextPalb]) {
+      pos--;
+      if (pos < 0) {
+        return `Error en el encadenamiento`;
+      }
+    }
+    for (let j: number = pos; j < palb.length; j++) {
+      if (palb[j] == nextPalb[posnextPalb]) {
+        output = output + palb[j];
+        posnextPalb++;
+      } else {
+        return `Error en el encadenamiento`;
+      }
+    }
+  }
+  return output;
+}
